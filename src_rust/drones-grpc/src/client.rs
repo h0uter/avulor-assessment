92e06a4060288f5a149c24749c9a673df27ce01e
W. Meijer <houterm@gmail.com>
2022-08-01 17:14:05 +0200

    rust client fully operational

--- src_rust/drones-grpc/src/client.rs
+++ src_rust/drones-grpc/src/client.rs
@@ -5,21 +5,59 @@ use drones_grpc::drones;
 use drones::greeter_client::GreeterClient;
 use drones::Registration;
 
+use std::{thread, time::Duration};
+
+
+struct MyDrone {
+    id: i32,
+    name: String,
+    lattitude: f32,
+    longitude: f32,
+    altitude: f32,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut my_drone = MyDrone {
+        id: 0,
+        name: "My Super Cool Drone".to_string(),
+        lattitude: 0.0,
+        longitude: 0.0,
+        altitude: 5.0,
+    };
+
     let addr = Endpoint::from_static("https://127.0.0.1:50051");
 
     // Registration
-    let drone_name = "My Super Cool Drone".to_string();
     let mut client  = GreeterClient::connect(addr).await?;
-    let request = Request::new(Registration{name: drone_name});
+    let request = Request::new(Registration{name: my_drone.name});
     let response = client.register(request).await?;
-    println!("response id: {}", response.into_inner().id);
+    my_drone.id = response.into_inner().id;
+    println!("response id: {}", my_drone.id);
 
     //sending position
     let pos_request = Request::new(drones::Position{latitude: 15.0, longitude: 20.0, altitude: 3.0});
     client.send_position(pos_request).await?;
 
+    //waypoint listening
+    let listen_request = Request::new(drones::Empty{});
+    let mut stream = client.listen_waypoint(listen_request).await?.into_inner();
+
+    while let Some(waypoint) = stream.message().await? {
+        println!("waypoint: {:?}", waypoint);
+
+        my_drone.lattitude = waypoint.latitude;
+        my_drone.longitude = waypoint.longitude;
+        
+        // wait 5 seconds for "drone to do its thing"
+        thread::sleep(Duration::from_secs(4));
+
+        //sending position update once the drone has reached its waypoint
+        let pos_request = Request::new(drones::Position{latitude: my_drone.lattitude, longitude: my_drone.longitude, altitude: my_drone.altitude});
+        client.send_position(pos_request).await?;
+
+    }
 
     Ok(())
 }
