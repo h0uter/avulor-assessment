e6dce54654b9d4442273fb5b5a660b7c0f0b9c50
W. Meijer <houterm@gmail.com>
2022-08-01 16:53:39 +0200

    rust client also does sending the pos

--- src_rust/drones-grpc/src/client.rs
+++ src_rust/drones-grpc/src/client.rs
@@ -2,32 +2,24 @@ use tonic::transport::Endpoint;
 use tonic::Request;
 
 use drones_grpc::drones;
-// use drones::drones_client::HelloClient;
 use drones::greeter_client::GreeterClient;
-// use drones::HelloRequest;
 use drones::Registration;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = Endpoint::from_static("https://127.0.0.1:50051");
 
+    // Registration
     let drone_name = "My Super Cool Drone".to_string();
-    
     let mut client  = GreeterClient::connect(addr).await?;
     let request = Request::new(Registration{name: drone_name});
     let response = client.register(request).await?;
     println!("response id: {}", response.into_inner().id);
 
+    //sending position
+    let pos_request = Request::new(drones::Position{latitude: 15.0, longitude: 20.0, altitude: 3.0});
+    client.send_position(pos_request).await?;
+
+
     Ok(())
 }
-
-// async fn main() -> Result<(), Box<dyn std::error::Error>> {
-//     let addr = Endpoint::from_static("https://127.0.0.1:50051");
-    
-//     let mut client  = HelloClient::connect(addr).await?;
-//     let request = Request::new(HelloRequest{});
-//     let response = client.hello_world(request).await?;
-//     println!("response: {}", response.into_inner().message);
-
-//     Ok(())
-// }
